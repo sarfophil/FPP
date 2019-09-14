3442bbd2b0970d7b8b093caee1fcd1a2269bf69f
sarfophil <philsarfogh@gmail.com>
2019-09-13 22:20:39 -0500

    Assignment 5 Upgrade

--- assignment5/Question5/Computer.java
+++ assignment5/Question5/Computer.java
@@ -1,51 +1,47 @@
-/**
- * 
- */
-package homework5;
+package demoine;
 
-/**
- * @author 610488
- *
- */
 public class Computer {
 	private String manufacturer;
 	private String processor;
 	private int ramSize;
 	private double processorSpeed;
-	
-	public Computer(String manufacturer,String processor,int ramSize,double processorSpeed) {
+
+	public Computer(String manufacturer, String processor, int ramSize, double processorSpeed) {
 		this.manufacturer = manufacturer;
 		this.processor = processor;
 		this.ramSize = ramSize;
 		this.processorSpeed = processorSpeed;
 	}
-	
+
 	public int getRamSize() {
 		return ramSize;
 	}
-	
+
 	public double getProcessorSpeed() {
 		return processorSpeed;
-	} 
-	
+	}
+
 	public double computePower() {
 		return ramSize * processorSpeed;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Manufacturer: "+manufacturer+" Processor:"+processor+" Ram Size: "+processorSpeed+" Power: "+this.computePower();
+		return "Manufacturer: " + manufacturer + " Processor:" + processor + " Ram Size: " + processorSpeed + " Power: "
+				+ this.computePower();
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
-		if(obj == null) return false;
-		if(getClass() != obj.getClass()) return false;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
 		Computer computer = (Computer) obj;
-		return computer.manufacturer.equals(computer.manufacturer) && computer.processor.equals(computer.processor);
+		return manufacturer.equals(computer.manufacturer) && processor.equals(computer.processor);
 	}
-	
-	
-	
-	
+
+	protected Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	};
 }
